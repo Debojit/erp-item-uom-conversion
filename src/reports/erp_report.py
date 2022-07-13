68ae1e648cf140553f631d48c2b01f519e304658
Debojit Sinha <sinha.debojit@gmail.com>
2022-07-13 03:04:02 +0000

    Reformat credential access for SOAP client

--- src/reports/erp_report.py
+++ src/reports/erp_report.py
@@ -1,10 +1,20 @@
+from dotenv import load_dotenv
+
 from soap_client import SoapClient
 
 import xmltodict
+
+import os
+
 class ErpReportClient(SoapClient):
 
-    def __init__(self, wsdl: str):
-        SoapClient.__init__(self, wsdl)
+    def __init__(self):
+        """Initalise ERP report SOAP client"""
+        load_dotenv('../../.env')
+        wsdl:str = 'https://' + os.environ.get('ERP_HOST') + ':' + os.environ.get('ERP_PORT')
+        user:str = os.environ.get('erp.user') if 'erp.user' in os.environ else None
+        password:str = os.environ.get('erp.password') if 'erp.password' in os.environ else None
+        SoapClient.__init__(self, wsdl, user, password)
 
     def run_report(self, payload: dict) -> dict:
         """Run ERP report for payload and return XML response"""
--- src/reports/soap_client.py
+++ src/reports/soap_client.py
@@ -11,16 +11,17 @@ import os
 
 class SoapClient:
     
-    def __init__(self, wsdl: str):
+    def __init__(self, wsdl: str, user:str = None, password:str = None):
         """Initialise a SOAP client with WSDL and optional username and password"""
-        load_dotenv('../../.env')
-        session = Session()
-        user = os.environ.get('ERP_USER')
-        password = os.environ.get('ERP_PASSWORD')
-        session.auth = HTTPBasicAuth(user, password)
+        session:Session = Session()
+
+        self.history:HistoryPlugin = HistoryPlugin()
         
-        self.history = HistoryPlugin() # Required for logging SOAP request/response payloads
-        self.client = Client(wsdl, transport=Transport(session=session), plugins=[self.history])
+        if user != None and password != None:
+            session.auth:HTTPBasicAuth = HTTPBasicAuth(user, password)
+            self.client:Client = Client(wsdl, transport=Transport(session=session), plugins=[self.history])
+        else:
+            self.client:Client = Client(wsdl, plugins=[self.history])
     
     def get_last_request(self):
         """Returns latest SOAP request"""
