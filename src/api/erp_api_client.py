becd18fec37304d1f31ede68258d8c7a13159a16
Debojit Sinha <sinha.debojit@gmail.com>
2022-07-14 14:23:36 +0000

    Intraclass update API client initial build

--- src/api/erp_api_client.py
+++ src/api/erp_api_client.py
@@ -1,3 +1,4 @@
+import resource
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 import requests
@@ -16,22 +17,46 @@ def _invoke_api(verb: str, resource_url: str, headers: Optional[Dict] = None, pa
 
     payload = json.dumps(payload)
 
-    api_url = 'https://' + os.getenv('erp.host') + ':' + os.getenv('erp.port') + resource_url
+    api_url = 'https://' + \
+        os.getenv('erp.host') + ':' + os.getenv('erp.port') + resource_url
     response = requests.request(method=verb, url=api_url, auth=HTTPBasicAuth(os.getenv('erp.user'),
-                                                                    os.getenv('erp.password')), headers={'Content-Type': 'application/json'}, data=payload)
+                                                                             os.getenv('erp.password')), headers={'Content-Type': 'application/json'}, data=payload)
     response.raise_for_status()
     return (response.status_code, response.json())
 
 
-def get_intraclass_conversions(uom_code: str, item_id: str) -> Optional[Dict]:
+def get_intraclass_conversions(uom_code: str, item_id: int) -> Dict:
     """Get interclass conversion for UOM Code and Item ID"""
 
     resource_url: str = f'/fscmRestApi/resources/11.13.18.05/unitsOfMeasure/{uom_code}/child/intraclassConversions?q=InventoryItemId={item_id}&onlyData=true'
     try:
-        status_code, response_payload = _invoke_api(verb='GET', resource_url=resource_url)
+        status_code, response_payload = _invoke_api(
+            verb='GET', resource_url=resource_url)
         return {
             'status_code': status_code,
             'data': response_payload['items']
+        }
+    except requests.exceptions.HTTPError as httpe:
+        return {
+            'status_code': httpe.response.status_code,
+            'error': f'Invalid UOM Code {uom_code}'
+        }
+
+
+def update_intraclass_conversion(conv_id: int, uom_code: str, item_id: int, conv_value: float) -> Dict:
+    """Update intraclass conversion for an item"""
+
+    resource_url: str = f'/fscmRestApi/resources/11.13.18.05/unitsOfMeasure/{uom_code}/child/intraclassConversions/{conv_id}'
+    request_payload: dict = {
+        'InventoryItemId': item_id,
+        'IntraclassConversion': conv_value
+    }
+    try:
+        status_code, response_payload = _invoke_api(
+            verb='PATCH', resource_url=resource_url, payload=request_payload)
+        return {
+            'status_code': status_code,
+            'data': response_payload
         }
     except requests.exceptions.HTTPError as httpe:
         return {
