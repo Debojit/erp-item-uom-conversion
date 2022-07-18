d337999538e9d50723ef38d988a6f5ec758f85b7
Debojit Sinha <sinha.debojit@gmail.com>
2022-07-18 13:18:05 +0000

    Modify intraclass usaer data error filtration logic.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser, Namespace
 import json
 
 
-def _convert_intraclass(report: dict) -> List[Dict]:
+def _convert_intraclass(report: Dict) -> List[Dict]:
     """Calculate intraclass UOM conversion(s) and load to ERP"""
 
     data = report['DATA_DS']['G_1']
@@ -16,10 +16,18 @@ def _convert_intraclass(report: dict) -> List[Dict]:
         data = [data]
 
     response_buffer:List[Dict] = []
-    error_items:List = [item for item in data if item['ERROR_FLAG'] == 'Y']
     for item in data:
-        if item in error_items:
-            continue # TODO: Load to response buffer
+        if item['ERROR_FLAG'] == 'Y':
+            response_buffer.append({
+                            'item_number': item['ITEM_NUMBER'],
+                            'item_desc': item['DESCRIPTION'],
+                            'conv_type': 'Intraclass',
+                            'from_uom': item['UNIT_OF_MEASURE'],
+                            'conversion': None,
+                            'to_uom': item['BASE_UOM_NAME'],
+                            'status': 'Error',
+                            'status': f'Intraclass conversion excluded as the UOM Class {item["CLASS_NAME"]} does not belong to Item Primary UOM Class which is {item["ITEM_PRIMARY_UOM_CLASS"]}'
+                        })
         else:
             item_id: str = item['INVENTORY_ITEM_ID']
             item_number: str = item['ITEM_NUMBER']
@@ -56,7 +64,7 @@ def _convert_intraclass(report: dict) -> List[Dict]:
                             'from_uom': from_uom_name,
                             'conversion': current_intraclass_conv,
                             'to_uom': to_uom_name,
-                            'status': 'Conversion Value Unchanged; Skipping'
+                            'status': 'Conversion unchanged; skipped'
                         })
                     continue
                 else:
@@ -111,6 +119,10 @@ def _convert_intraclass(report: dict) -> List[Dict]:
     return response_buffer
 
 
+def _convert_interclass(report: Dict) -> List[Dict]:
+    pass
+
+
 def item_uom_conversion(item_number: str, item_class: str) -> None:
     """Create Item UOM conversion data for item number and/or class"""
     # Convert Intraclass
@@ -123,12 +135,15 @@ def item_uom_conversion(item_number: str, item_class: str) -> None:
         request['reportRequest']['parameterNameValues'][0]['item'][1]['values'][0]['item'] = item_class
         request['reportRequest']['parameterNameValues'][0]['item'][2]['values'][0]['item'] = 'INTRACLASS'
 
-    # Fetch Report Data
     client = ErpReportClient()
+    # Convert Intraclass
     intraclass_report = client.run_report(request)
-    _convert_intraclass(intraclass_report)
-    # Convert Interclass
+    intraclass_conversions = _convert_intraclass(intraclass_report)
 
+    # Convert Interclass
+    request['reportRequest']['parameterNameValues'][0]['item'][2]['values'][0]['item'] = 'INTERCLASS'
+    interclass_report = client.run_report(request)
+    interclass_conversions = _convert_interclass(interclass_report)
     # Format Output
 
 
