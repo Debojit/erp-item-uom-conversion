f29c1848b727be284a9aa277615066cf8a38a204
Debojit Sinha <sinha.debojit@gmail.com>
2022-07-26 13:20:19 +0000

    Format tabluated command-line output

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from api import erp_api_client as client
 from typing import List, Dict
 from argparse import ArgumentParser, Namespace
 
+from tabulate import tabulate
 import json
 
 
@@ -31,7 +32,7 @@ def _convert_intraclass(conversion: Dict) -> Dict:
 
     # Check if conversion data exists.
     if intraclass_conv_data['status_code'] == 200 \
-        and intraclass_conv_data['data'] != {}:
+            and intraclass_conv_data['data'] != {}:
         conversion_data: Dict = intraclass_conv_data['data']
         current_intraclass_conv: float = conversion_data['conversion_value']
 
@@ -39,18 +40,20 @@ def _convert_intraclass(conversion: Dict) -> Dict:
         # skip to next conversion item
         if current_intraclass_conv == intraclass_conv:
             return {
-                    'item_number': item_number,
-                    'item_desc': item_desc,
-                    'conv_type': 'Intraclass',
-                    'from_uom': from_uom_name,
-                    'conversion': current_intraclass_conv,
-                    'to_uom': to_uom_name,
-                    'status': 'Conversion unchanged; skipped'
-                }
+                'item_number': item_number,
+                'item_desc': item_desc,
+                'conv_type': 'Intraclass',
+                'from_uom': from_uom_name,
+                'conversion': current_intraclass_conv,
+                'to_uom': to_uom_name,
+                'status': 'Success',
+                'message': 'Conversion value identical; skipped'
+            }
         else:
             conversion_id: int = conversion_data['conversion_id']
             conversion_update_data: Dict = \
-                client.update_intraclass_conversion(conversion_id, uom_code, item_id, intraclass_conv)
+                client.update_intraclass_conversion(
+                    conversion_id, uom_code, item_id, intraclass_conv)
             if conversion_update_data['status_code'] == 200:
                 return {
                     'item_number': item_number,
@@ -74,28 +77,29 @@ def _convert_intraclass(conversion: Dict) -> Dict:
                 }
     else:  # If conversion does not exist, call create API
         conversion_create_data = \
-            client.create_intraclass_conversion(uom_code, item_id, intraclass_conv)
+            client.create_intraclass_conversion(
+                uom_code, item_id, intraclass_conv)
         if conversion_create_data['status_code'] == 201:
             return {
-                    'item_number': item_number,
-                    'item_desc': item_desc,
-                    'conv_type': 'Intraclass',
-                    'from_uom': from_uom_name,
-                    'conversion': conversion_create_data['data']['conversion_value'],
-                    'to_uom': to_uom_name,
-                    'status': 'Success'
-                }
+                'item_number': item_number,
+                'item_desc': item_desc,
+                'conv_type': 'Intraclass',
+                'from_uom': from_uom_name,
+                'conversion': conversion_create_data['data']['conversion_value'],
+                'to_uom': to_uom_name,
+                'status': 'Success'
+            }
         else:
             return {
-                    'item_number': item_number,
-                    'item_desc': item_desc,
-                    'conv_type': 'Intraclass',
-                    'from_uom': from_uom_name,
-                    'conversion': intraclass_conv,
-                    'to_uom': to_uom_name,
-                    'status': 'Error',
-                    'error_message': conversion_create_data['error']
-                }
+                'item_number': item_number,
+                'item_desc': item_desc,
+                'conv_type': 'Intraclass',
+                'from_uom': from_uom_name,
+                'conversion': intraclass_conv,
+                'to_uom': to_uom_name,
+                'status': 'Error',
+                'error_message': conversion_create_data['error']
+            }
 
 
 def _convert_interclass(conversion: Dict) -> List[Dict]:
@@ -110,18 +114,20 @@ def _convert_interclass(conversion: Dict) -> List[Dict]:
     to_uom_name: str = conversion['TO_UOM_NAME']
     primary_uom_class: str = conversion['ITEM_PRIMARY_UOM_CLASS_CODE']
     conversion_value: float = float(conversion['UOM_CONV_VALUE'])
-    from_conveersion_rate:float = float(conversion['FROM_CONV_RATE'])
+    from_conveersion_rate: float = float(conversion['FROM_CONV_RATE'])
     to_conversion_rate: float = float(conversion['TO_CONV_RATE'])
     to_uom_class: str = conversion['TO_CLASS_NAME']
-    
-    interclass_conversion = from_conveersion_rate / (conversion_value * to_conversion_rate)
+
+    interclass_conversion = from_conveersion_rate / \
+        (conversion_value * to_conversion_rate)
 
     # Check if conversion exists
     interclass_conv_data: Dict = \
-        client.get_interclass_conversions(primary_uom_class, item_id, from_uom_code, to_uom_code)
-    
+        client.get_interclass_conversions(
+            primary_uom_class, item_id, from_uom_code, to_uom_code)
+
     if interclass_conv_data['status_code'] == 200 and \
-        interclass_conv_data['data'] != {}:
+            interclass_conv_data['data'] != {}:
         conversion_data: Dict = interclass_conv_data['data']
         current_interclass_conv: float = conversion_data['conversion_value']
 
@@ -135,12 +141,14 @@ def _convert_interclass(conversion: Dict) -> List[Dict]:
                 'from_uom': from_uom_name,
                 'conversion': current_interclass_conv,
                 'to_uom': to_uom_name,
-                'status': 'Conversion unchanged; skipped'
+                'status': 'Success',
+                'message': 'Conversion value identical; skipped'
             }
         else:
             conversion_id = conversion_data['conversion_id']
             conversion_update_data: Dict = \
-                client.update_interclass_conversion(primary_uom_class, conversion_id, from_uom_code, to_uom_code, interclass_conversion)
+                client.update_interclass_conversion(
+                    primary_uom_class, conversion_id, from_uom_code, to_uom_code, interclass_conversion)
             if conversion_update_data['status_code'] == 200:
                 return {
                     'item_number': item_number,
@@ -164,17 +172,18 @@ def _convert_interclass(conversion: Dict) -> List[Dict]:
                 }
     else:
         conversion_create_data: Dict = \
-            client.create_interclass_conversion(to_uom_class, item_number, from_uom_code, to_uom_code, interclass_conversion)
+            client.create_interclass_conversion(
+                to_uom_class, item_number, from_uom_code, to_uom_code, interclass_conversion)
         if conversion_create_data['status_code'] == 200:
             return {
-                    'item_number': item_number,
-                    'item_desc': item_desc,
-                    'conv_type': 'Interclass',
-                    'from_uom': from_uom_name,
-                    'conversion': conversion_create_data['data']['conversion_value'],
-                    'to_uom': to_uom_name,
-                    'status': 'Success'
-                }
+                'item_number': item_number,
+                'item_desc': item_desc,
+                'conv_type': 'Interclass',
+                'from_uom': from_uom_name,
+                'conversion': conversion_create_data['data']['conversion_value'],
+                'to_uom': to_uom_name,
+                'status': 'Success'
+            }
         else:
             return {
                 'item_number': item_number,
@@ -208,7 +217,7 @@ def item_uom_conversion(item_number: str, item_class: str) -> None:
     try:
         intraclass_data: List[Dict] = intraclass_report['DATA_DS']['G_1']
         for item in intraclass_data:
-            if item['ERROR_FLAG'] == 'Y': # Skip items with data error
+            if item['ERROR_FLAG'] == 'Y':  # Skip items with data error
                 intraclass_conversions.append({
                     'item_number': item['ITEM_NUMBER'],
                     'item_desc': item['DESCRIPTION'],
@@ -222,19 +231,47 @@ def item_uom_conversion(item_number: str, item_class: str) -> None:
             else:
                 conversion_data = _convert_intraclass(item)
                 intraclass_conversions.append(conversion_data)
-    except KeyError: 
+    except KeyError:
         pass
 
     # Convert Interclass
     request['reportRequest']['parameterNameValues'][0]['item'][2]['values'][0]['item'] = 'INTERCLASS'
     interclass_report = client.run_report(request)
+    interclass_conversions: List[Dict] = []
     try:
         interclass_data: List[Dict] = interclass_report['DATA_DS']['G_2']
         for item in interclass_data:
             conversion_data = _convert_interclass(item)
+            interclass_conversions.append(conversion_data)
     except KeyError:
         pass
+
     # Format Output
+    table_headers_success = ['Item Number', 'Description', 'Conversion Type',
+                             'From Base UOM', 'Conversion', 'To Base UOM', 'Message']
+    table_headers_error = ['Item Number', 'Description', 'Conversion Type',
+                           'From Base UOM', 'Conversion', 'To Base UOM', 'Error Message']
+
+    interclass_conv_success: List = [[item['item_number'], item['item_desc'], item['conv_type'], item['from_uom'],
+                                      item['conversion'], item['to_uom'], item['message']] for item in interclass_conversions if item['status'] == 'Success']
+    interclass_conv_error: List = [[item['item_number'], item['item_desc'], item['conv_type'], item['from_uom'],
+                                    item['conversion'], item['to_uom'], item['status']] for item in interclass_conversions if item['status'] == 'Error']
+
+    intraclass_conv_success: List = [[item['item_number'], item['item_desc'], item['conv_type'], item['from_uom'],
+                                      item['conversion'], item['to_uom'], item['message']] for item in intraclass_conversions if item['status'] == 'Success']
+    intraclass_conv_error: List = [[item['item_number'], item['item_desc'], item['conv_type'], item['from_uom'],
+                                    item['conversion'], item['to_uom'], item['status']] for item in intraclass_conversions if item['status'] == 'Error']
+
+    print('--Intraclass Conversions--')
+    print(tabulate(intraclass_conv_success,
+          headers=table_headers_success, tablefmt="grid"))
+    print(tabulate(intraclass_conv_error,
+          headers=table_headers_error, tablefmt="grid"))
+    print('--Interclass Conversions--')
+    print(tabulate(interclass_conv_success,
+          headers=table_headers_success, tablefmt="grid"))
+    print(tabulate(interclass_conv_error,
+          headers=table_headers_error, tablefmt="grid"))
 
 
 if __name__ == '__main__':
